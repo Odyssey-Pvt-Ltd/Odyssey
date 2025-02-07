e4b1bcd6cd93f2f06c7355af746bcfae5fc67549
Amiru46 <165811854+Amiru46@users.noreply.github.com>
2025-02-07 22:45:42 +0530

    spring boot initializr commit

---
+++ src/test/java/com/Odyssey/Odyssey/OdysseyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Odyssey.Odyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdysseyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
