79ab14756e799412528effe3fa2a3af7d652c030
binnyboi45 <binara.20230503@iit.ac.lk>
2025-03-08 14:45:19 +0530

    Vendor class updated (add variables and annotaions) to initiale vendor as valid jpa entity

--- backend/src/main/java/com/Odyssey/Odyssey/model/Vendor.java
+++ backend/src/main/java/com/Odyssey/Odyssey/model/Vendor.java
@@ -1,4 +1,29 @@
 package com.Odyssey.Odyssey.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="vendors")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Vendor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String Name;
+
+    private String Email;
+
+    private String Phone;
+
+    private String Address;
+
 }
