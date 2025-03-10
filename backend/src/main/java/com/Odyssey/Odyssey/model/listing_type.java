275054ac3b190de137e4147906ed7e3c45bd9753
binnyboi45 <binara.20230503@iit.ac.lk>
2025-03-10 16:32:39 +0530

    created enum classes listing type and user role

---
+++ backend/src/main/java/com/Odyssey/Odyssey/model/listing_type.java
@@ -0,0 +1,5 @@
+package com.Odyssey.Odyssey.model;
+
+public enum listing_type {
+    VEHICLES,HOTELS,RESTUARANTS;
+}
---
+++ backend/src/main/java/com/Odyssey/Odyssey/model/user_role.java
@@ -0,0 +1,5 @@
+package com.Odyssey.Odyssey.model;
+
+public enum user_role {
+    ROLE_USER,ROLE_VENDOR,ROLE_ADMIN;
+}
