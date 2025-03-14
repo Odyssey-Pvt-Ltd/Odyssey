0fc64db5821b408b92e587035c999204606a8494
Amiru46 <165811854+Amiru46@users.noreply.github.com>
2025-03-14 18:05:56 +0530

    fix error

--- backend/src/main/java/com/Odyssey/Odyssey/config/JwtTokenValidator.java
+++ backend/src/main/java/com/Odyssey/Odyssey/config/JwtTokenValidator.java
@@ -36,7 +36,7 @@ public class JwtTokenValidator extends OncePerRequestFilter {
 
                 SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
 
-                Claims claims = Jwts.parserBuilder().setSigningKey(key).parseClaimsJws(jwt).getBody();
+                Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
 
                 String email = String.valueOf(claims.get("email"));
                 String authorities = String.valueOf(claims.get("authorities"));
