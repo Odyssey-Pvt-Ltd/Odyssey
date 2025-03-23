4bf400d2c3811385e9c73201622895e5f66d905f
binnyboi45 <binara.20230503@iit.ac.lk>
2025-03-23 15:07:22 +0530

    testing pipeline

--- backend/src/main/java/com/Odyssey/Odyssey/config/SecurityConfig.java
+++ backend/src/main/java/com/Odyssey/Odyssey/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
-//hyuio
+//hyuio 234
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
