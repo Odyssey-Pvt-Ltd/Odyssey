8c184a0ef12dfa7478e29288eff11e8285b40e4b
Amiru46 <165811854+Amiru46@users.noreply.github.com>
2025-03-23 12:15:06 +0530

    up

--- backend/src/main/java/com/Odyssey/Odyssey/controller/CategoryController.java
+++ backend/src/main/java/com/Odyssey/Odyssey/controller/CategoryController.java
@@ -1,20 +1,47 @@
 package com.Odyssey.Odyssey.controller;
 
 
+import com.Odyssey.Odyssey.model.Category;
+import com.Odyssey.Odyssey.model.User;
 import com.Odyssey.Odyssey.service.CategoryService;
+import com.Odyssey.Odyssey.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/admin/category")
+@RequestMapping("/api")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    public ResponseEntity<Category>
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("admin/category")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category,
+                                                   @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        Category createdCategory = categoryService.createCategory(category.getName(), user.getId());
+        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/category/shop")
+    public ResponseEntity<Category> getShopCategory(@RequestBody Category category,
+                                                   @RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserByJwtToken(jwt);
+
+        Category createdCategory = categoryService.findCategoryById(category.getId());
+        return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+    }
+
+
 
 
 }
