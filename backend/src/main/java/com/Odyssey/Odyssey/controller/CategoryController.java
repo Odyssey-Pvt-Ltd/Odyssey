28315bb4b966dec5b680c0047e91821d67b992a7
Amiru46 <165811854+Amiru46@users.noreply.github.com>
2025-03-23 11:53:19 +0530

    error fix

--- backend/src/main/java/com/Odyssey/Odyssey/controller/CategoryController.java
+++ backend/src/main/java/com/Odyssey/Odyssey/controller/CategoryController.java
@@ -1,10 +1,17 @@
 package com.Odyssey.Odyssey.controller;
 
 
+import com.Odyssey.Odyssey.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin/category")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+
 }
--- backend/src/main/java/com/Odyssey/Odyssey/service/CategoryService.java
+++ backend/src/main/java/com/Odyssey/Odyssey/service/CategoryService.java
@@ -1,12 +1,14 @@
 package com.Odyssey.Odyssey.service;
 
 import com.Odyssey.Odyssey.model.Category;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface CategoryService {
 
-    public Category createCategory(String name, Long user_id);
+    public Category createCategory(String name, Long user_id) throws Exception;
 
     public List<Category> findCategoryByShopId(Long id) throws Exception;
 
